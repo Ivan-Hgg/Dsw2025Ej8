5710e7daae0ec07431f41bb30ce0b4f14ec5fbdb
Favio Tadeo Moya <faviotadeomoya@gmail.com>
2025-05-02 14:39:04 -0300

    Cambios mas recientes de Tadeo

--- Domain/CuentaCorriente.cs
+++ Domain/CuentaCorriente.cs
@@ -4,9 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Dsw2025Ej8.Domain
+namespace Dsw2025Ej8.Domain;
+
+class CuentaCorriente
 {
-    internal class CuentaCorriente
+    public decimal LimiteDeDescubierto { get; init; }
+
+    public CuentaCorriente(string numoer, decimal saldo, string[] titulares) : base(numero, saldo, titulares)
     {
     }
 }
